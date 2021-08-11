42573cc7968dfd3bd1fc5b241a9fd7870460567c
Aniket <Aniket@DESKTOP-KDQFS4P>
2021-08-11 23:25:25 +0530

    upload all

---
+++ Java project/src/Employee.java
@@ -0,0 +1,17 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		int a=50;
+		if(a<10) {
+			System.out.println("a is less than 10");
+		}
+		else if(a==10) {
+			System.out.println("a is equal to 10");
+		}
+		else {
+         System.out.println("a is gretaer tha 10");
+	}
+ }	
+
+}
